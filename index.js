c6ce39f095572b7eccf0b3066fbeb5ff7c1cf851
Sadman9061 <sadmansakib.dev@gmail.com>
2023-11-09 14:22:27 +0600

    User Authentication Routes: Create routes and controllers for user registration and login.

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ app.use(cors(
 }
 ));
 app.use(express.json());
+// ffffffffffffffffffff
 // Connect With MongoDb Database
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.urcdkb0.mongodb.net/?retryWrites=true&w=majority`;
